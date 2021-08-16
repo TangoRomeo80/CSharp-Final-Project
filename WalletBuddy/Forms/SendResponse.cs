da5bbb54d25a14ece57d12bdcd95d4077e856e5d
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-08-16 10:45:37 +0600

    Update SendResponse.cs

--- WalletBuddy/Forms/SendResponse.cs
+++ WalletBuddy/Forms/SendResponse.cs
@@ -37,10 +37,10 @@ namespace WalletBuddy.Forms
       }
       else
       {
-        string senderMail = "tanzeemRahatVal@gmail.com";
+        string senderMail = "";
         string subject = "Response to your feedback from Wallet Buddy";
         string credentialName = "";
-        string credentialPassword = "rahatOTG2769";
+        string credentialPassword = "";
         MailMessage mail = new MailMessage(senderMail, eMail, subject, descriptionTextBox.Texts);
         SmtpClient client = new SmtpClient("smtp.gmail.com");
         client.UseDefaultCredentials = false;
